1307d4d52805b4ae34ed807765a735ac7f86e759
pcurve <y7t7jvn2dr@privaterelay.appleid.com>
2026-08-18 08:33:47 +0200

    sldprt: test disc22 disc20 keyed use lattice

--- crates/cadmpeg-codec-sldprt/src/brep/entity.rs
+++ crates/cadmpeg-codec-sldprt/src/brep/entity.rs
@@ -9478,6 +9478,7 @@ mod tests {
     mod disc22_disc1a_disc20_disc1e_disc16;
     mod disc22_disc1e_disc1c_disc1a_disc18_disc10;
     mod disc22_disc20_disc1a_disc18_disc10;
+    mod disc22_disc20_disc1e_disc18_disc16_disc0e_disc04;
     mod disc22_disc20_disc1e_disc1a;
     mod disc22_disc20_disc1e_disc1a_disc18_disc16;
     mod disc22_disc20_disc1e_disc1a_disc18_disc16_disc14_disc12_disc06;
